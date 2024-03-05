490b75b6f77aac5e460eebe4ccd3e9ed1a714d3e
angelrcx <hielorc@gmail.com>
2024-03-05 15:03:47 -0600

    Creacion del programa

---
+++ Consecionaria/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Consecionaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
